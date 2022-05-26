7830eddfe8cfe3a486db03d8defc7bb3bff1711b
madoka@home <guoqunabc@126.com>
2022-05-26 08:58:14 +0800

    .

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math
 import jieba
+import wordcloud
+import stylecloud
 import pandas as pd
 import plotly.express as px
 
@@ -21,6 +23,19 @@ def barrage_parse(path_file):
     return _df
 
 
+def ciyun(text_input, file_output):
+    stylecloud.gen_stylecloud(
+        text=text_input,  # 上面分词的结果作为文本传给text参数
+        size=512,
+        max_words=300,
+        font_path='msyh.ttc',  # 字体设置
+        palette='cartocolors.qualitative.Pastel_7',  # 调色方案选取，从palettable里选择
+        gradient='horizontal',  # 渐变色方向选了垂直方向
+        # icon_name='fab fa-weixin',  # 蒙版选取，从Font Awesome里选
+        output_name=file_output  # 输出词云图
+    )
+    return
+
 path_barrage_1 = os.path.join(dir_raw, '1.xml')
 path_barrage_2 = os.path.join(dir_raw, '2.xml')
 df_1 = barrage_parse(path_barrage_1)
@@ -32,16 +47,11 @@ print(df.shape)
 
 list_kouling = ['一代神U', 'LCD永不为奴', 'Redmi联名阿童木', '512G版本来了', '全体起立']
 df = df[~df['sentence'].isin(list_kouling)].reset_index(drop=True)
+print(df.shape)
 
 tqdm.pandas()
-# df['snownlp'] = df.progress_apply(lambda row: SnowNLP(row['sentence']), axis=1)
-# df['分词'] = df['snownlp'].apply(lambda x: x.words)
-# df['情感'] = df['snownlp'].apply(lambda x: x.sentiments)
 df['情感'] = df.progress_apply(lambda row: SnowNLP(row['sentence']).sentiments, axis=1)
-# df['分词'] = df['sentence'].apply(lambda x: SnowNLP(x).sentiments)
-# df['情感'] = df['sentence'].apply(lambda x: SnowNLP(x).sentiments)
-df['情感'].mean()
-df['情感'].describe()
+df['分词'] = df.progress_apply(lambda row: SnowNLP(row['sentence']).words, axis=1)
 
 df['minute'] = df['time'].apply(lambda x: math.floor(x / 60))
 df['second'] = df['time'].apply(lambda x: math.floor(x))
@@ -51,21 +61,42 @@ df_pvt_minute.columns = ['弹幕条数/分']
 df_pvt_minute['分钟'] = df_pvt_minute.index
 
 fig = px.line(df_pvt_minute, x='分钟', y='弹幕条数/分')
-path_html = os.path.join(dir_img, '弹幕条数-分.html')
-fig.write_html(path_html)
+path_fig = os.path.join(dir_img, '弹幕条数-分')
+fig.write_html(f'{path_fig}.html')
+fig.write_image(f'{path_fig}.png')
 
 df_pvt_second = pd.pivot_table(df, index='second', values=['sentence'], aggfunc=['count'])
 df_pvt_second.columns = ['弹幕条数/秒']
 df_pvt_second['秒钟'] = df_pvt_second.index
 
 fig = px.line(df_pvt_second, x='秒钟', y='弹幕条数/秒')
-path_html = os.path.join(dir_img, '弹幕条数-秒.html')
-fig.write_html(path_html)
+path_fig = os.path.join(dir_img, '弹幕条数-秒')
+fig.write_html(f'{path_fig}.html')
+fig.write_image(f'{path_fig}.png')
 
 df_pvt_minute = pd.pivot_table(df, index='minute', values=['情感'], aggfunc=['mean'])
 df_pvt_minute.columns = ['弹幕情感/分']
 df_pvt_minute['分钟'] = df_pvt_minute.index
 
 fig = px.line(df_pvt_minute, x='分钟', y='弹幕情感/分')
-path_html = os.path.join(dir_img, '弹幕情感-分.html')
-fig.write_html(path_html)
+path_fig = os.path.join(dir_img, '弹幕情感-分')
+fig.write_html(f'{path_fig}.html')
+fig.write_image(f'{path_fig}.png')
+
+df_pvt_second = pd.pivot_table(df, index='second', values=['情感'], aggfunc=['mean'])
+df_pvt_second.columns = ['弹幕情感/秒']
+df_pvt_second['秒钟'] = df_pvt_second.index
+
+fig = px.line(df_pvt_second, x='秒钟', y='弹幕情感/秒')
+path_fig = os.path.join(dir_img, '弹幕情感-秒')
+fig.write_html(f'{path_fig}.html')
+fig.write_image(f'{path_fig}.png')
+
+## 词云测试
+contents = []
+[contents.extend(_) for _ in df['分词']]
+# words_remove = ['中', '中中', '啊', '哈哈']
+# contents = [_ for _ in contents if _ not in words_remove]
+contents = ' '.join(contents)
+path_res = os.path.join(dir_res, 'test.png')
+ciyun(contents, path_res)
